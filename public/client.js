444a079b8759ce537acbaf60de0fdfa07c1c37e4
kevin <jaimerpena1@gmail.com>
2025-03-26 16:17:41 -0500

    finishing the functionality and displaying the data onto the page

--- public/client.js
+++ public/client.js
@@ -7,8 +7,48 @@ function geoFindMe() {
         const api_url = '/weather/' + latitude + '/' + longitude;
         const response = await fetch(api_url);
         const json = await response.json();
-        console.log(json);
-        status.textContent = json.forecast.forecastday[0].date;
+        status.textContent = "";
+
+        const weekday = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday",];
+        var array = document.getElementsByClassName("day-container");
+        console.log(array);
+
+        for (var i = 0; i < array.length; i++){
+            var weather = json.forecast.forecastday[i].day.condition.text;
+            var weather_icon = "https:" + json.forecast.forecastday[0].day.condition.icon;
+
+            var high = json.forecast.forecastday[i].day.maxtemp_f;
+            var low = json.forecast.forecastday[i].day.mintemp_f;
+
+            var day_date = json.forecast.forecastday[i].date;
+            var d = new Date(day_date);
+            let day = weekday[d.getDay()];
+
+            
+            var container_children = array[i].getElementsByTagName("p");
+            array[i].querySelector(".weather-icon").src = weather_icon;
+            console.log(container_children);
+            
+            for (var j = 0; j < container_children.length;j++){
+                switch(container_children[j].className){
+                      case "day-name": 
+                          container_children[j].innerHTML = day;
+                          break;
+                      case "weather-info high": 
+                          container_children[j].innerHTML = "High: " + high + " F";
+                          break;
+                      case "weather-info low":
+                          container_children[j].innerHTML = "Low: "+ low + " F";
+                          break;
+                      case "weather-info weather":
+                          container_children[j].innerHTML = weather;
+                          break;
+                      default:
+                          console.log("error happened somewhere");
+                          
+                  }
+            }
+        }
     }
   
     function error() {
@@ -24,4 +64,5 @@ function geoFindMe() {
   }
   
 document.querySelector("#find-me").addEventListener("click", geoFindMe);
+
 
